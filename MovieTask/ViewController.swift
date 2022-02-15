745026edad9d8df1ad6d48509408126651d07253
Mohamed Aglan <mohamedaglan2225@gmail.com>
2022-02-15 20:12:22 +0200

    Initial Commit

---
+++ MovieTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTask
+//
+//  Created by Mohamed Aglan on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
